53d44b41d0c47c2115f4f0e5ad3ce55fea829b46
Ligre <ligreman@users.noreply.github.com>
2018-02-10 21:54:28 +0100

    Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {RouterModule} from '@angular/router';
+import {MatButtonModule, MatIconModule} from '@angular/material';
 
 import {AppComponent} from './app.component';
 import {ROUTING} from './app.routing';
 import {CalcComponent} from './calc/calc.component';
 import {WordComponent} from './word/word.component';
 import {HomeComponent} from './home/home.component';
-import { NotfoundComponent } from './notfound/notfound.component';
+import {NotfoundComponent} from './notfound/notfound.component';
 
 @NgModule({
     declarations: [
@@ -23,7 +24,9 @@ import { NotfoundComponent } from './notfound/notfound.component';
         BrowserModule,
         FormsModule,
         HttpClientModule,
-        ROUTING
+        ROUTING,
+        MatButtonModule,
+        MatIconModule
     ],
     providers: [],
     bootstrap: [AppComponent]
